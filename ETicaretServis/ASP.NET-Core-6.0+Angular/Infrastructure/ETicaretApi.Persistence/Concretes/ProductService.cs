7ee74ed0daedf3935c68aab36a2a47e86050aa59
Emre DİREK <38289108+emreddirek09@users.noreply.github.com>
2022-11-18 23:47:52 +0300

    Onion Architecture Mimari Testi Gerçekleşti

---
+++ ETicaretServis/ASP.NET-Core-6.0+Angular/Infrastructure/ETicaretApi.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using ETicaretApi.Application.Abstractions;
+using ETicaretApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApi.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts() => new()
+        {
+            new (){Id = Guid.NewGuid(),Name="Product 1",Price=100,Stock="10",CreateDate=DateTime.Now},
+            new (){Id = Guid.NewGuid(),Name="Product 2",Price=101,Stock="10",CreateDate=DateTime.Now},
+            new (){Id = Guid.NewGuid(),Name="Product 3",Price=102,Stock="10",CreateDate=DateTime.Now},
+            new (){Id = Guid.NewGuid(),Name="Product 4",Price=103,Stock="10",CreateDate=DateTime.Now}
+        };
+    }
+}
