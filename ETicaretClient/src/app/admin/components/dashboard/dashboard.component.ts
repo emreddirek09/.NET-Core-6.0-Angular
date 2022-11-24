8e1a899c1d78924c42f7896e77b792e0e901c7c1
Emre DİREK <38289108+emreddirek09@users.noreply.github.com>
2022-11-24 17:17:48 +0300

    Alertify İle Özelleştirilmiş Notification

--- ETicaretClient/src/app/admin/components/dashboard/dashboard.component.ts
+++ ETicaretClient/src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+ 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+ 
+   constructor(private alertify:AlertifyService){}
+  ngOnInit(): void {  
+ 
+ 
+    // alertify.success('Success message');
+  }
+  m(){
+    this.alertify.message("Merhaba",{
+      messageType:MessageType.Error,
+      delay:5,
+      position:Position.BottomLeft
+    })
+  }
 
+  d(){ 
+    this. alertify.dismiss();
+  }
 }
